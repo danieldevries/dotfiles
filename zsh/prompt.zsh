65c59131dead8c58e57cc3077c24f11607a4523e
Daniël de Vries <contact@danieldevries.eu>
2025-09-12 11:45:37 +0200

    Updated ZSH config

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,7 +9,7 @@ zstyle ':vcs_info:*' stagedstr ' +'
 zstyle ':vcs_info:git:*' formats       '(%b%u%c)'
 zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
 
-PROMPT="%{$fg[green]%}#%{$reset_color%} %{$fg[yellow]%}%~%{$reset_color%} %{$fg[red]%}->%{$reset_color%} "
+PROMPT="%{$fg[green]%}#%{$reset_color%} %{$fg[yellow]%}%~%{$reset_color%} %{$fg[red]%}>%{$reset_color%} "
 RPROMPT='%(?..%F{red}(%?%)%f)%b%B%F{green}${vcs_info_msg_0_}%f'
 
 # https://salferrarello.com/zsh-git-status-prompt/
